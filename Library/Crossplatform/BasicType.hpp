76ec4d9293129ab010b31b3eb0fc5b3d3b634369
Ya ping Jin <freecnjet@gmail.com>
2018-07-14 11:42:31 +0800

    Update BasicType.hpp

--- Library/Crossplatform/BasicType.hpp
+++ Library/Crossplatform/BasicType.hpp
@@ -7,35 +7,4 @@ __BEGIN_NAME_SPACE__
  */ 
 typedef unsigned char Byte;
 
-
-
-
-/**
- * Define standard component, com/corba similar
- */
-typedef u64  GUID;
-typedef long HResult;
- 
-interface IComponent {
-    /**
-     * Cast current component to another component, automatically call acquire when success.
-     */
-    virtual HResult STD_CALL Cast(/*in*/GUID tid, /*out*/IComponent** pComponent) = 0;
-    
-    /**
-     * Acquire component manually to avoid it release when still using it.
-     */
-    virtual u32 STD_CALL Acquire() = 0;
-    
-    /**
-     * Release component, it can check to destroy.
-     */
-    virtual u32 STD_CALL Release() = 0;
-};
-
-interface IComponentFactory {
-    virtual HResult STD_CALL CreateComponent(/*in*/GUID tid, /*out*/IComponent** pComponent) = 0;
-};
-
-
 __END_NAME_SPACE__
